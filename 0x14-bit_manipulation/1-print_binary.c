d6ade1c4e6be1ba3976fa85e8851bca28b6705fa
Amanda-Khebevu <amandakhebevu@gmail.com>
2023-09-01 17:22:33 -0700

    1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * decimal_binary - print in binary with bitwise operator
+ * @n: decimal number
+ * Return: void - just prints
+ */
+void decimal_binary(unsigned long int n)
+{
+	if (n == 0)
+		return;
+
+	decimal_binary(n >> 1);
+	n = n & 1;
+	_putchar(n + 48);
+}
+
+/**
+ * print_binary - Function convert into binary
+ * @n: number to convert
+ */
+void print_binary(unsigned long int n)
+{
+	if (n == 0)
+		_putchar('0');
+	else
+		decimal_binary(n);
+}
